f73b8a04ee49fcbef910f9287bd35c4232519db3
rodrigo toledo <rodrigo@rtoledo.inf.br>
2025-11-14 23:04:43 -0300

    Update Ruby and Node versions, adjust comment prepend target

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,6 @@ class Comment < ApplicationRecord
   after_create :broadcast_comment
 
   def broadcast_comment
-    broadcast_prepend_to [ post, "comments" ], target: "comments", partial: "comments/comment", locals: { comment: self }
+    broadcast_prepend_to [ post, "comments" ], target: "#{post.id}_comments", partial: "comments/comment", locals: { comment: self }
   end
 end
